a8f2d0544422cb15a8d01857200a780fd4ac3ec2
DrSaadLa <data16scientist@gmail.com>
2023-10-19 07:45:46 +0300

    Add Ex_01 file.

---
+++ Exercises/Ex_01.rs
@@ -0,0 +1,34 @@
+// Exercise 1: Hello, World!
+// -------------------------
+
+// 1. Create a Rust program that prints "Hello, Rust!" to the console.
+// 2. Create a Rust program that prints "Hello, your full-name".
+
+// Hints:
+// - You can use the `println!` macro to print text to the console.
+
+// Note:
+// 1. The main() function body should contain a call to other functions.
+// 2. Do not copy and paste, type the solutions manually.
+
+
+// -----------------------------------------------------------------------------
+//                      Your solution goes here.
+// -----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+// Recall the main function syntax
+// -------------------------------
+// fn main() {
+//     // Your solution
+//     // testfunc()
+// }
+
