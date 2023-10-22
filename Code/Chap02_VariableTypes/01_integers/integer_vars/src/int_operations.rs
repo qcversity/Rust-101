b5ea8d7475a76a613b8d60c744bf9ccd7913e222
DrSaadLa <data16scientist@gmail.com>
2023-10-22 05:54:45 +0300

    Add int operations file

---
+++ Code/Chap02_VariableTypes/01_integers/integer_vars/src/int_operations.rs
@@ -0,0 +1,34 @@
+// Function to add two integers and return the result.
+pub fn add_ints() {
+    let a = 10;
+    let b = 5;
+    println!("The sum of two ints is {}", a + b)
+}
+
+// Function to subtract the second integer from the first and return the result.
+pub fn sub_ints() {
+    let a = 10;
+    let b = 5;
+    println!("The subtraction result is {}", a - b)
+}
+
+// Function to multiply two integers and return the result.
+pub fn mul_ints() {
+    let a = 10;
+    let b = 5;
+    println!("The multiplication results is {}", a * b)
+}
+
+// Function to divide the first integer by the second and return the result.
+pub fn divide_ints() {
+    let a = 7;
+    let b = 3;
+    println!("The integer division result is {}", (a / b))
+}
+
+// Function to calculate the remainder when the first integer is divided by the second.
+pub fn remainder_ints() {
+    let a = 7;
+    let b = 3;
+    println!("The remainder of the division is {}", (a % b));
+}
--- Code/Chap02_VariableTypes/01_integers/integer_vars/src/main.rs
+++ Code/Chap02_VariableTypes/01_integers/integer_vars/src/main.rs
@@ -1,3 +1,4 @@
+mod int_operations;
 mod rnd_integers;
 
 fn main() {
@@ -15,16 +16,24 @@ fn main() {
     make_separator();
 
     // Generate a random signed integers
-    rnd_integers::print_rnd_integers()
+    rnd_integers::print_rnd_integers();
+
+    // Integer Operations
+    make_separator();
+    int_operations::add_ints();
+    int_operations::sub_ints();
+    int_operations::mul_ints();
+    int_operations::divide_ints();
+    int_operations::remainder_ints();
+    make_separator();
 }
 
 // Simple function that prints `=` to pretify the console output
 fn make_separator() {
-    println!("{}", "=".repeat(52));
+    println!("{}", "*".repeat(52));
 }
 
 // a function that prints signed integers
-
 fn print_signed_integers() {
     let i8_val: i8 = -123;
     let i16_val: i16 = -12345;
@@ -43,12 +52,27 @@ fn signed_integers_size() {
     let i32_val: i32 = -12345678;
     let i64_val: i64 = -1234567890;
 
-    println!("i8 : {:<12} (Size: {} bytes)", i8_val, std::mem::size_of::<i8>());
-    println!("i16: {:<12} (Size: {} bytes)", i16_val, std::mem::size_of::<i16>());
-    println!("i32: {:<12} (Size: {} bytes)", i32_val, std::mem::size_of::<i32>());
-    println!("i64: {:<12} (Size: {} bytes)", i64_val, std::mem::size_of::<i64>());
+    println!(
+        "i8 : {:<12} (Size: {} bytes)",
+        i8_val,
+        std::mem::size_of::<i8>()
+    );
+    println!(
+        "i16: {:<12} (Size: {} bytes)",
+        i16_val,
+        std::mem::size_of::<i16>()
+    );
+    println!(
+        "i32: {:<12} (Size: {} bytes)",
+        i32_val,
+        std::mem::size_of::<i32>()
+    );
+    println!(
+        "i64: {:<12} (Size: {} bytes)",
+        i64_val,
+        std::mem::size_of::<i64>()
+    );
 }
-
 
 // A function that print unsigned integers
 fn print_unsigned_integers() {
@@ -69,10 +93,24 @@ fn unsigned_integers_size() {
     let u32_val: u32 = 12345678;
     let u64_val: u64 = 1234567890;
 
-    println!("u8: {:<12} (Size: {} bytes)", u8_val, std::mem::size_of::<u8>());
-    println!("u16: {:<12} (Size: {} bytes)", u16_val, std::mem::size_of::<u16>());
-    println!("u32: {:<12} (Size: {} bytes)", u32_val, std::mem::size_of::<u32>());
-    println!("u64: {:<12} (Size: {} bytes)", u64_val, std::mem::size_of::<u64>());
+    println!(
+        "u8: {:<12} (Size: {} bytes)",
+        u8_val,
+        std::mem::size_of::<u8>()
+    );
+    println!(
+        "u16: {:<12} (Size: {} bytes)",
+        u16_val,
+        std::mem::size_of::<u16>()
+    );
+    println!(
+        "u32: {:<12} (Size: {} bytes)",
+        u32_val,
+        std::mem::size_of::<u32>()
+    );
+    println!(
+        "u64: {:<12} (Size: {} bytes)",
+        u64_val,
+        std::mem::size_of::<u64>()
+    );
 }
-
-
