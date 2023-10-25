33ffaa253fc55924872ae7de0215231cba669e2a
DrSaadLa <data16scientist@gmail.com>
2023-10-25 06:21:34 +0300

    Add the boolean chapter.

--- Code/Chap02_VariableTypes/03_booleans/bool_vars/src/main.rs
+++ Code/Chap02_VariableTypes/03_booleans/bool_vars/src/main.rs
@@ -1,3 +1,107 @@
+mod practice;
+use practice::practice_with_bools;
+
 fn main() {
-    println!("Hello, world!");
+    make_separator();
+    boolean_vars();
+
+    make_separator();
+    bool_vars_size();
+
+    make_separator();
+    convert_bool_vars();
+
+    make_separator();
+    logical_and();
+    
+    make_separator();
+    logical_or();
+
+    make_separator();
+    logical_not();
+
+    // Check the result on the console
+    // then read the source code (writing functions will be in a separate chapter)
+    make_separator();
+    practice_with_bools();
+    
+    make_separator()
+}
+
+// A function that checks boolean variables
+fn boolean_vars() {
+    let is_ok: bool = true; // lowercase true
+    let is_bad: bool = false; // lowercase false
+
+    println!(
+        "The boolean variables in Rust are: {} and {}",
+        is_ok, is_bad
+    );
+}
+
+fn bool_vars_size() {
+    let x: bool = true;
+    let y: bool = false;
+    println!(
+        "The size of {} is {} bytes.",
+        x,
+        std::mem::size_of::<bool>()
+    );
+    println!(
+        "The size of {} is {} bytes.",
+        y,
+        std::mem::size_of::<bool>()
+    );
+}
+
+// Converting Booleans
+// Boolean can be converted to integers using the `as` keyword
+// true will be converted to 1
+// false will be converted to 0
+
+fn convert_bool_vars() {
+    let x: bool = true;
+    let y: bool = false;
+
+    let x_to_one: i32 = x as i32; // The as keyword performs the conversion
+    let y_to_zero: i32 = y as i32;
+
+    println! {"The conversion of {} is {}", x, x_to_one};
+    println! {"The conversion of {} is {}", y, y_to_zero};
+}
+
+// Logical Operators:
+//  1. &&: And operator
+//  2. ||: Or operator
+
+// Function to perform a logical AND operation
+fn logical_and() {
+    let a: bool = true;
+    let b: bool = false;
+    println!("{:<6} and {:<5} is ==> {}", a, a, a && a);
+    println!("{:<6} and {:<5} is ==> {}", a, b, a && b);
+    println!("{:<6} and {:<5} is ==> {}", b, b, b && b);
+}
+
+// Function to perform a logical OR operation
+fn logical_or() {
+    let a: bool = true;
+    let b: bool = false;
+    println!("{:<6} or {:<5} is ==> {}", a, a, a || a);
+    println!("{:<6} or {:<5} is ==> {}", a, b, a || b);
+    println!("{:<6} or {:<5} is ==> {}", b, b, b || b);
+}
+
+// The logical not `!`
+
+fn logical_not() {
+    let a: bool = true;
+    let b: bool = false;
+    println!("a is {:<6} and not a is {:<5}", a, !a);
+    println!("b is {:<6} and not b is {:<5}", b, !b);
+}
+
+// Simple function for formatting the output in the console
+fn make_separator() {
+    println!("{}", "*".repeat(52));
 }
---
+++ Code/Chap02_VariableTypes/03_booleans/bool_vars/src/practice.rs
@@ -0,0 +1,52 @@
+// Function to check if a number is even
+
+pub fn is_even(number: i32) -> bool {
+    number % 2 == 0
+}
+
+// Function to check if a number is prime
+pub fn is_prime(number: u32) -> bool {
+    if number <= 1 {
+        return false;
+    }
+    for i in 2..(number / 2 + 1) {
+        if number % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+// Logical operators
+
+// Function to perform a logical AND operation
+fn logical_and(a: bool, b: bool) -> bool {
+    a && b
+}
+
+// Function to perform a logical OR operation
+fn logical_or(a: bool, b: bool) -> bool {
+    a || b
+}
+pub fn practice_with_bools() {
+    let numbers = 1..=7; // Create a range of values from 1 to 9 (inclusive).
+
+    for number in numbers {
+        let result = is_even(number);
+        println!("Is {} even? {}", number, result);
+    }
+    println!("{}", "*".repeat(52));
+    let numbers_2 = (1..=17).step_by(2);
+    for number in numbers_2 {
+        let is_prime_num = is_prime(number);
+        println!("Is {} a prime number? {}", number, is_prime_num);
+    }
+    println!("{}", "*".repeat(52));
+    let a = true;
+    let b = false;
+    let and_result = logical_and(a, b);
+    println!("Logical AND: {:<10} && {:<10} = {}", a, b, and_result);
+
+    let or_result = logical_or(a, b);
+    println!("Logical OR : {:<10} || {:<10} = {}", a, b, or_result);
+}
+
